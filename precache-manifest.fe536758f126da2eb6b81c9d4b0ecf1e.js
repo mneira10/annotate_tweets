38a09dea900d25b54c1d94a8107b6f7e23cf2fc0
Mauricio Neira <m.neira10@uniandes.edu.co>
2019-04-01 03:21:46 -0500

    Updates

---
+++ precache-manifest.fe536758f126da2eb6b81c9d4b0ecf1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed057711dd885dfe7b6a",
+    "url": "/annotate_tweets/static/js/runtime~main.a8363f01.js"
+  },
+  {
+    "revision": "f7955782b25a1263b883",
+    "url": "/annotate_tweets/static/js/main.b814e94c.chunk.js"
+  },
+  {
+    "revision": "52ce56270f5cffac2852",
+    "url": "/annotate_tweets/static/js/2.e43b3bb5.chunk.js"
+  },
+  {
+    "revision": "f7955782b25a1263b883",
+    "url": "/annotate_tweets/static/css/main.d6e56ce5.chunk.css"
+  },
+  {
+    "revision": "2bdc033a762007aebf16c610f7f8e8ec",
+    "url": "/annotate_tweets/index.html"
+  }
+];
